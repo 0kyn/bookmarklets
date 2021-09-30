1eade7172600c2b33a187051aa97ecaf4ca429d7
0kyn <0kyn.dev@gmail.com>
2021-09-30 03:01:51 +0200

    feat: add github first commits

---
+++ github-first-commits.js
@@ -0,0 +1,23 @@
+/* Get the first 10 commits page of a github repository 
+Widely inspired by @bpceee https://github.com/bpceee/oldest */
+(async function ([_, repo, branch]) {
+    const repoUrlBase = `${location.origin}/${repo}`;
+    let dom = document;
+    if (typeof branch === 'undefined') {
+        const repoBasePageContent = await (await fetch(repoUrlBase)).text();
+        dom = new DOMParser().parseFromString(repoBasePageContent, 'text/html');
+        const mainBranch = dom.querySelector("#branch-select-menu .css-truncate-target").textContent;
+        branch = mainBranch
+    }
+
+    const repoUrlTree = `${repoUrlBase}/tree/${branch}`;
+    if (location.href !== repoUrlTree) {
+        const repoTreePageContent = await (await fetch(repoUrlTree)).text();
+        dom = new DOMParser().parseFromString(repoTreePageContent, 'text/html');
+    }
+
+    const commitsCount = parseInt(dom.querySelector('.d-none.d-sm-inline > strong').textContent.replace(',', ''));
+    const commitId = dom.querySelector('.f6.Link--secondary').getAttribute('href').split('/').pop();
+    
+    location.href = `${location.origin}/${repo}/commits/${branch}?after=${commitId}+${commitsCount - 10}&branch=${branch}`;
+})(location.pathname.match(/\/([^\/]+\/[^\/]+)(?:\/(?:tree|commits|blob)\/([^\/]+))?/));
