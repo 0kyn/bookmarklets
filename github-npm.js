77ce116b9f2ee40914a889e4c256076536431e53
0kyn <0kyn.dev@gmail.com>
2021-11-11 13:51:05 +0100

    feat: open npmjs.com package of a github repo

---
+++ github-npm.js
@@ -0,0 +1,22 @@
+/* Open the npmjs.com package page of a Github repository */
+(async function ([_, repo, branch]) {
+    const repoUrlBase = `${location.origin}/${repo}`;
+    let dom = document;
+    if (typeof branch === 'undefined') {
+        const repoBasePageContent = await (await fetch(repoUrlBase)).text();
+        dom = new DOMParser().parseFromString(repoBasePageContent, 'text/html');
+        const mainBranch = dom.querySelector('#branch-select-menu .css-truncate-target').textContent;
+        branch = mainBranch;
+    }
+    const packageJsonRawUrl = `https://raw.githubusercontent.com/${repo}/${branch}/package.json`;
+    const packageJsonContent = await (await fetch(packageJsonRawUrl)).text();
+    const packageName = packageJsonContent.match(/{(?:\r|\n|[^{])+(?:\s|\t)+"name".*"(.*)"/);
+    if(packageName){
+        const npmJsUrl = `https://www.npmjs.com/package/${packageName[1]}`;
+        window.open(npmJsUrl, '_blank');
+    } else {
+        const msg = 'Sorry no package name found.';
+        console.error(msg);
+        alert(msg);
+    }
+})(location.pathname.match(/\/([^/]+\/[^/]+)(?:\/(?:tree|commits|blob)\/([^/]+))?/));
