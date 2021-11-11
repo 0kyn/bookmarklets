fe80b8b67bb4f0204e104364f96dc7f6cc1318c6
0kyn <0kyn.dev@gmail.com>
2021-11-11 16:37:21 +0100

    feat: add redirect to github repo

---
+++ redirect-to/github.js
@@ -0,0 +1,26 @@
+/* This script attempts to redirect you to the github repository of the current library */
+(function(){
+    const aTags = {
+        'npmjs.com': document.querySelectorAll('a[aria-labelledby="repository"]')[0],
+        'snyk.io': document.querySelectorAll('.meta > .item > a[href*="github.com"]')[0],
+        'npmtrends.com': document.querySelectorAll('a[href*="github.com"]')[0],
+        'bundlephobia.com': document.querySelectorAll('a.quick-stats-bar__link[href*="github.com"]')[0],
+        'packagist.org': document.querySelector('.details > p.canonical > a'),
+        'pypi.org': document.querySelectorAll('.vertical-tabs__list a[href*="github.com"]')[0],
+        'rubygems.org': document.querySelectorAll('a[href*="github.com"]')[0],
+        'conan.io': document.querySelectorAll('.v-window a[href*="github.com"]')[0],
+    };
+
+    const domainSplit = document.domain.split('.');
+    const domain = domainSplit[domainSplit.length - 2] + '.' + domainSplit[domainSplit.length - 1];
+    const repoLink = aTags[domain];
+
+    if(repoLink) {
+        const githubRepoUrl = repoLink.getAttribute('href');
+        window.open(githubRepoUrl, '_blank');
+    } else {
+        const msg = 'Sorry no Github repository found.';
+        console.error(msg);
+        alert(msg);
+    }
+})();
