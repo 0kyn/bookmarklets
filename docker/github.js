c468fb93d4f584a100951662778b56ad90a4ebc0
0kyn <0kyn.dev@gmail.com>
2022-06-12 13:39:27 +0200

    fix: when the first link is not the repo home page

--- docker/github.js
+++ docker/github.js
@@ -1,10 +1,16 @@
-/* Open the current dockerhub image Github repository */
+/* Open the current dockerhub image Github repository 
+@todo? : regexp product name in github url */
 (function(){
-    const repoLink = document.querySelector('[class*="styles__productDescription"] a[href*="github.com"]');
-    if(repoLink) {
-        const githubRepoUrl = repoLink.getAttribute('href');
-        window.open(githubRepoUrl, '_blank');
-    } else {
+    const githubLinks = document.querySelectorAll('[class*="styles__productDescription"] a[href*="github.com"]');
+    let found = false;
+    [... githubLinks].map(link => {
+        const href = link.getAttribute('href');
+        if(href.match(/github\.com\/[^/]+\/[^/]+$/g)){
+            found = true;
+            window.open(href, '_blank');
+        }
+    });
+    if(!found){
         const msg = 'Sorry no Github repository found.';
         console.error(msg);
         alert(msg);
