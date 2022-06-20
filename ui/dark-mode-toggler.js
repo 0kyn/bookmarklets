b36aaef6d331af6d1f643ddb89138024f4d5fbee
0kyn <0kyn.dev@gmail.com>
2022-06-20 16:36:45 +0200

    chore: remove useless parenthesis

--- ui/dark-mode-toggler.js
+++ ui/dark-mode-toggler.js
@@ -2,7 +2,7 @@
 (function(){
     const darkModeStyleTag = document.querySelector('#darkMode0');
     if(!darkModeStyleTag){
-        document.body.innerHTML+=('<style id="darkMode0">html{filter:invert(1) hue-rotate(180deg);}img,video,picture,*[style*="background-image"]{filter:invert(1) hue-rotate(180deg);}</style>');
+        document.body.innerHTML+='<style id="darkMode0">html{filter:invert(1) hue-rotate(180deg);}img,video,picture,*[style*="background-image"]{filter:invert(1) hue-rotate(180deg);}</style>';
     } else {
         darkModeStyleTag.parentNode.removeChild(darkModeStyleTag);
     }
